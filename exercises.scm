0ff14ff6ae85ab98a25958d5478a45afd66ce243
Alen Mujezinovic <flashingpumpkin@gmail.com>
2011-07-03 19:34:58 +0100

    More numbers games

--- exercises.scm
+++ exercises.scm
@@ -222,3 +222,80 @@
     (cond 
       ((< n m) 0)
       (else (add1 (/ (o- n m) m))))))
+
+(define length
+  (lambda (lat)
+    (cond 
+      ((null? lat) 0)
+      (else 
+       (add1 (length (cdr lat)))))))
+
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else
+       (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond 
+      ((zero? (sub1 n)) (cdr lat))
+      (else
+       (cons (car lat) 
+             (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) (quote ()))
+      (else
+       (cond 
+         ((number? (car lat)) (no-nums (cdr lat)))
+         (else 
+          (cons (car lat) (no-nums (cdr lat)))))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond 
+      ((null? lat) (quote ()))
+      (else
+       (cond
+         ((number? (car lat))
+          (cons (car lat)
+                (all-nums (cdr lat))))
+         (else
+          (all-nums (cdr lat))))))))
+          
+(define eqan?
+  (lambda (a b)
+    (cond
+      ((and (number? a) (number? b))
+       (= a b))
+      ((or (number? a) (number? b))
+       #f)
+      (else
+       (eq? a b)))))
+
+(define occur
+  (lambda (a lat)
+    (cond 
+      ((null? lat) 0)
+      (else
+       (cond
+         ((eq? a (car lat))
+          (add1 (occur a (cdr lat))))
+         (else (occur a (cdr lat))))))))
+
+(define one?
+  (lambda (a)
+    (= a 1)))
+
+(define rempick2
+  (lambda (n lat)
+    (cond
+      ((one? n) (cdr lat))
+      (else (cons (car lat)
+                  (rempick2 (sub1 n) (cdr lat)))))))
+
