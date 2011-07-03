5c9684da3c102f9ea666b2af91a747c3a310f991
Alen Mujezinovic <flashingpumpkin@gmail.com>
2011-07-03 19:09:17 +0100

    Numbers games

--- exercises.scm
+++ exercises.scm
@@ -133,3 +133,92 @@
           (cons (car lat)
                 (multisubst new old (cdr lat)))))))))
 
+
+;; Numbers games
+
+(define add1
+  (lambda (n)
+    (+ n 1)))
+
+(define sub1
+  (lambda (n)
+    (- n 1)))
+
+(define o+ 
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else
+       (add1 (o+ n (sub1 m)))))))
+
+(define o-
+  (lambda (n m)
+    (cond
+      ((zero? m) n)
+      (else
+       (sub1 (o- n (sub1 m)))))))
+
+; Sum a tuple
+(define addtup
+  (lambda (tup)
+    (cond 
+      ((null? tup) 0)
+      (else (+ (car tup)
+               (addtup (cdr tup)))))))
+
+; Multiplication
+(define * 
+  (lambda (n m)
+    (cond
+      ((zero? m) 0)
+      (else 
+       (+ n (* n (sub1 m)))))))
+
+; Sum two tuples into one
+(define tup+ 
+  (lambda (tup1 tup2)
+    (cond
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else (cons (+ (car tup1) (car tup2))
+                  (tup+ (cdr tup1) (cdr tup2)))))))
+
+(define >
+  (lambda (n m)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (> (sub1 n) (sub1 m))))))
+
+(define <
+  (lambda (n m)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (< (sub1 n) (sub1 m))))))
+
+(define = 
+  (lambda (n m)
+    (cond 
+      ((zero? m) (zero? n))
+      ((zero? n) #f)
+      (else (= (sub1 n) (sub1 m))))))
+
+(define =2
+  (lambda (n m)
+    (cond 
+      ((> n m) #f)
+      ((< n m) #f)
+      (else #t))))
+
+(define ^
+  (lambda (n m)
+    (cond 
+      ((zero? m) 1)
+      (else (* n (^ n (sub1 m)))))))
+
+(define /
+  (lambda (n m)
+    (cond 
+      ((< n m) 0)
+      (else (add1 (/ (o- n m) m))))))
